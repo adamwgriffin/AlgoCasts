03c9a6f8c87dcb3c28f97f971759ea03713e311f
Adam Griffin <adam.griffin@moxiworks.com>
2021-09-11 22:03:30 -0700

    Finished steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,39 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log('#'.repeat(i).padEnd(n));
+  }
+}
+
+
+function stepsNoRepeatPadEnd(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(
+      Array(i).fill('#').join('') + Array(n-i).fill(' ').join('')
+    );
+  }
+}
+
+function stepsOnlyForLoops(n) {
+  for (let row = 0; row < n; row++) {
+    let step = '';
+    for (let column = 0; column < n; column++) {
+      step += (column <= row) ? '#' : ' ';
+    }
+    console.log(step);
+  }
+}
+
+function stepsArrayFromForEach(n) {
+  Array.from({ length: n })
+    .forEach((_, i) => console.log('#'.repeat(i+1).padEnd(n)));
+}
+
+function stepsArryKeysForEach(n) {
+  [...Array(n).keys()]
+    .forEach((s) => console.log('#'.repeat(s+1).padEnd(n))); 
+}
 
 module.exports = steps;
