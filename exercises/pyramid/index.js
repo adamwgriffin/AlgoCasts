6f8b56abac92488c22d3e4742d50fd1a11bad5ac
Adam Griffin <adam.griffin@moxiworks.com>
2021-09-13 13:10:06 -0700

    Finished pyramid exercise

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,7 +13,46 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//      '   #   '
+//      '  ###  '
+//      ' ##### '
+//      '#######'
 
-function pyramid(n) {}
+// calculate chars for last row. each row needs two extra characters to form a pyramid, so:
+// (n - 1) * 2 = max
+// calculates the index of the middle character in each row
+// const midpoint = Math.floor((2 * n - 1) / 2)
+
+function pyramid(n) {
+  const rowCharCount = 2 * n - 1;
+  for (let row = 1, column = 1; row <= n; row++, column += 2) {
+    const pad = ' '.repeat((rowCharCount - column) / 2);
+    console.log(pad+'#'.repeat(column)+pad);
+  }
+}
+
+// without using repeat()
+function pyramidNoRepeat(n) {
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  const rowCharCount = 2 * n - 1;
+
+  for (let row = 0; row < n; row++) {
+    let level = '';
+    for (let column = 0; column < rowCharCount; column++) {
+      level += (midpoint - row <= column && midpoint + row >= column) ? '#' : ' ';
+    }
+    console.log(level);
+  }
+}
+
+// recursive version
+function pyramidRecursive(n, row=0, column=1) {
+  if (row === n) return;
+  const rowCharCount = 2 * n - 1;
+  const pad = ' '.repeat((rowCharCount - column) / 2);
+  console.log(pad+'#'.repeat(column)+pad);
+  pyramid(n, row + 1, column + 2);
+}
 
 module.exports = pyramid;
