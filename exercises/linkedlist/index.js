f2068231193b69b8deed718fe485860ae32ecc73
Adam Griffin <adam.griffin@moxiworks.com>
2021-11-07 16:56:35 -0800

    Completed removeAt method for LinkedList class

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -1,9 +1,112 @@
 // --- Directions
 // Implement classes Node and Linked Lists
 // See 'directions' document
+// file:///Users/ag/projects/algorithm-class/exercises/linkedlist/directions.html
 
-class Node {}
+class Node {
+  constructor(data, next=null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let node = this.head;
+    let count = 0;
+    while(node) {
+      count += 1;
+      node = node.next;
+    }
+    return count;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    let node = this.head;
+    while(node?.next) {
+      node = node.next;
+    }
+    return node;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (this.head) this.head = this.head.next;
+  }
+  
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+    let previous = this.head;
+    let node = this.head.next;
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+  }
+  
+  insertLast(data) {
+    let node = this.getLast();
+    if (node) {
+      node.next = new Node(data);
+    } else {
+      this.head = new Node(data);
+    }
+  }
+
+  getAt(index) {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+
+      counter++;
+      node = node.next;
+    }
+    return null;
+  }
+
+  removeAt(index) {
+    if (!this.head) {
+      return;
+    }
+
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+
+    const previous = this.getAt(index - 1);
+    if (!previous || !previous.next) {
+      return;
+    }
+    previous.next = previous.next.next;
+  }
+
+}
 
 module.exports = { Node, LinkedList };
